afc06803ca1a6da8b42b60c11e33aafe70a31be1
Kaushal Dokania <kaushal.dokania@microfocus.com>
2021-07-23 14:27:34 +0530

    configured Spring Security Authorization

--- src/main/java/org/kaushal/springbootsecurity/SecurityConfiguration.java
+++ src/main/java/org/kaushal/springbootsecurity/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package org.kaushal.springbootsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,9 +20,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .password("password")
                 .roles("USER")
                 .and()
-                .withUser("user2")
-                .password("password2")
+                .withUser("admin")
+                .password("adminpassword")
                 .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Go from most restrictive(admin) to least restrictive(all users)
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/").permitAll()
+//               .antMatchers("/", "static/css", "static/js").permitAll()      // this is how base url should be configured
+                .and().formLogin();
     }
 
     @Bean
